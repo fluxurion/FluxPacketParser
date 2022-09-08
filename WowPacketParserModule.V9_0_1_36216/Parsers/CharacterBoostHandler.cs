fc4c3c9e979f7341af38903a0d63b2a7889bd05d
Fluxurion <zoltan@fatalerror.hu>
2022-09-08 22:55:29 +0200

    Comment out charboost handler testing

---
+++ WowPacketParserModule.V9_0_1_36216/Parsers/CharacterBoostHandler.cs
@@ -0,0 +1,23 @@
+/*
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Proto;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+using System.Collections.Generic;
+
+namespace WowPacketParserModule.V9_0_1_36216.Parsers
+{
+
+    public static class CharacterBoostHandler
+    {
+        [Parser(Opcode.CMSG_CHARACTER_UPGRADE_START)]
+        public static void HandleBattlePayGetDistributionListResponse(Packet packet)
+        {
+            packet.ReadUInt32("Int1");
+        }
+
+    }
+}
+*/
