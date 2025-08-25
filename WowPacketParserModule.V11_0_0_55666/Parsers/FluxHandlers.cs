02bfa49b1c376fc60b9751923092b8b3265f2210
fluxurion <zoltan@fatalerror.hu>
2025-08-25 08:22:51 +0200

    Added CMSG_SETUP_WARBAND_GROUPS 11.2

---
+++ WowPacketParserModule.V11_0_0_55666/Parsers/FluxHandlers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V11_0_0_55666.Parsers
+{
+    public static class WarbandHandler
+    {
+        [Parser(Opcode.CMSG_SETUP_WARBAND_GROUPS)]
+        public static void HandleSetupWarbandGroups(Packet packet)
+        {
+            var accountGroupUnlockFlag = packet.ReadByte("AccountGroupUnlockFlag");
+
+            for (var i = 0; i < accountGroupUnlockFlag / 8; ++i)
+            {
+                packet.ResetBitReader();
+
+                packet.ReadUInt64("GroupID");
+                packet.ReadByte("OrderIndex");
+                if (ClientVersion.AddedInVersion(ClientVersionBuild.V11_1_0_59347))
+                    packet.ReadUInt32("WarbandSceneID");
+
+                packet.ReadInt32("Flags");
+                var memberCount = packet.ReadUInt32();
+                for (var j = 0u; j < memberCount; ++j)
+                    ReadWarbandGroupMember(packet, "Members", j);
+
+                if (ClientVersion.AddedInVersion(ClientVersionBuild.V11_1_0_59347))
+                {
+                    var nameLength = packet.ReadBits(9);
+                    packet.ReadWoWString("Name", nameLength);
+                }
+            }
+        }
+
+        public static void ReadWarbandGroupMember(Packet packet, params object[] idx)
+        {
+            packet.ReadInt32("WarbandScenePlacementID", idx);
+            var type = packet.ReadInt32("Type", idx);
+            if (type == 0)
+                packet.ReadPackedGuid128("Guid", idx);
+        }
+
+        public static void ReadWarbandGroup(Packet packet, params object[] idx)
+        {
+            packet.ReadUInt64("GroupID", idx);
+            packet.ReadByte("OrderIndex", idx);
+            packet.ReadInt32("Flags", idx);
+            var memberCount = packet.ReadUInt32();
+            for (var i = 0u; i < memberCount; ++i)
+                ReadWarbandGroupMember(packet, idx, "Members", i);
+        }
+    }
+}
