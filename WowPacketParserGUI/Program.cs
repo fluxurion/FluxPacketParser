f3a779e84e50382fc7974b496d875b6af8782006
Fluxurion <zoltan@fatalerror.hu>
2026-01-24 09:31:57 +0100

    Implement WowPacketParser GUI

---
+++ WowPacketParserGUI/Program.cs
@@ -0,0 +1,11 @@
+namespace WowPacketParserGUI;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
