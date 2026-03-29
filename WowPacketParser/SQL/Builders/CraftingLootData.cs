1c6ee85badce617306c0e41c27a5ee216e89537b
fluxurion <zoltan@fatalerror.hu>
2026-03-29 13:33:13 +0200

    Add loot template storage and crafting handler cleanup
    - Add config keys for 6 new loot template types (reference, scrapping, item, prospecting, milling, spell)
    - Add corresponding SQLOutput enum entries and DataBag storage for loot templates
    - Remove FluxCraftingHandler.cs (moved crafting packet parsing elsewhere)

---
+++ WowPacketParser/SQL/Builders/CraftingLootData.cs
@@ -0,0 +1,95 @@
+using System.Linq;
+using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Store;
+
+namespace WowPacketParser.SQL.Builders
+{
+    [BuilderClass]
+    public static class CraftingLootData
+    {
+        [BuilderMethod]
+        public static string ReferenceLootTemplateData()
+        {
+            if (Storage.ReferenceLootTemplates.IsEmpty())
+                return string.Empty;
+
+            if (!Settings.SQLOutputFlag.HasAnyFlagBit(SQLOutput.reference_loot_template))
+                return string.Empty;
+
+            var templateDb = SQLDatabase.Get(Storage.ReferenceLootTemplates);
+
+            return SQLUtil.Compare(Settings.SQLOrderByKey ? Storage.ReferenceLootTemplates.OrderBy(x => x.Item1.Entry).ThenBy(x => x.Item1.Item).ToArray() : Storage.ReferenceLootTemplates.ToArray(), templateDb, x => string.Empty);
+        }
+
+        [BuilderMethod]
+        public static string ScrappingLootTemplateData()
+        {
+            if (Storage.ScrappingLootTemplates.IsEmpty())
+                return string.Empty;
+
+            if (!Settings.SQLOutputFlag.HasAnyFlagBit(SQLOutput.scrapping_loot_template))
+                return string.Empty;
+
+            var templateDb = SQLDatabase.Get(Storage.ScrappingLootTemplates);
+
+            return SQLUtil.Compare(Settings.SQLOrderByKey ? Storage.ScrappingLootTemplates.OrderBy(x => x.Item1.Entry).ThenBy(x => x.Item1.Item).ToArray() : Storage.ScrappingLootTemplates.ToArray(), templateDb, x => string.Empty);
+        }
+
+        [BuilderMethod]
+        public static string ItemLootTemplateData()
+        {
+            if (Storage.ItemLootTemplates.IsEmpty())
+                return string.Empty;
+
+            if (!Settings.SQLOutputFlag.HasAnyFlagBit(SQLOutput.item_loot_template))
+                return string.Empty;
+
+            var templateDb = SQLDatabase.Get(Storage.ItemLootTemplates);
+
+            return SQLUtil.Compare(Settings.SQLOrderByKey ? Storage.ItemLootTemplates.OrderBy(x => x.Item1.Entry).ThenBy(x => x.Item1.Item).ToArray() : Storage.ItemLootTemplates.ToArray(), templateDb, x => string.Empty);
+        }
+
+        [BuilderMethod]
+        public static string ProspectingLootTemplateData()
+        {
+            if (Storage.ProspectingLootTemplates.IsEmpty())
+                return string.Empty;
+
+            if (!Settings.SQLOutputFlag.HasAnyFlagBit(SQLOutput.prospecting_loot_template))
+                return string.Empty;
+
+            var templateDb = SQLDatabase.Get(Storage.ProspectingLootTemplates);
+
+            return SQLUtil.Compare(Settings.SQLOrderByKey ? Storage.ProspectingLootTemplates.OrderBy(x => x.Item1.Entry).ThenBy(x => x.Item1.Item).ToArray() : Storage.ProspectingLootTemplates.ToArray(), templateDb, x => string.Empty);
+        }
+
+        [BuilderMethod]
+        public static string MillingLootTemplateData()
+        {
+            if (Storage.MillingLootTemplates.IsEmpty())
+                return string.Empty;
+
+            if (!Settings.SQLOutputFlag.HasAnyFlagBit(SQLOutput.milling_loot_template))
+                return string.Empty;
+
+            var templateDb = SQLDatabase.Get(Storage.MillingLootTemplates);
+
+            return SQLUtil.Compare(Settings.SQLOrderByKey ? Storage.MillingLootTemplates.OrderBy(x => x.Item1.Entry).ThenBy(x => x.Item1.Item).ToArray() : Storage.MillingLootTemplates.ToArray(), templateDb, x => string.Empty);
+        }
+
+        [BuilderMethod]
+        public static string SpellLootTemplateData()
+        {
+            if (Storage.SpellLootTemplates.IsEmpty())
+                return string.Empty;
+
+            if (!Settings.SQLOutputFlag.HasAnyFlagBit(SQLOutput.spell_loot_template))
+                return string.Empty;
+
+            var templateDb = SQLDatabase.Get(Storage.SpellLootTemplates);
+
+            return SQLUtil.Compare(Settings.SQLOrderByKey ? Storage.SpellLootTemplates.OrderBy(x => x.Item1.Entry).ThenBy(x => x.Item1.Item).ToArray() : Storage.SpellLootTemplates.ToArray(), templateDb, x => string.Empty);
+        }
+    }
+}
---
+++ WowPacketParser/Store/Objects/CraftingLootTemplates.cs
@@ -0,0 +1,239 @@
+using WowPacketParser.Misc;
+using WowPacketParser.SQL;
+
+namespace WowPacketParser.Store.Objects
+{
+    [DBTableName("reference_loot_template")]
+    public class ReferenceLootTemplate : IDataModel
+    {
+        [DBFieldName("Entry", true)]
+        public uint Entry;
+
+        [DBFieldName("Item", true)]
+        public uint Item;
+
+        [DBFieldName("Reference", true)]
+        public int Reference;
+
+        [DBFieldName("Chance")]
+        public float Chance;
+
+        [DBFieldName("QuestRequired")]
+        public bool QuestRequired;
+
+        [DBFieldName("LootMode")]
+        public ushort LootMode;
+
+        [DBFieldName("GroupId")]
+        public byte GroupId;
+
+        [DBFieldName("MinCount")]
+        public byte MinCount;
+
+        [DBFieldName("MaxCount")]
+        public byte MaxCount;
+
+        [DBFieldName("Comment")]
+        public string Comment;
+
+        [DBFieldName("VerifiedBuild")]
+        public int VerifiedBuild = ClientVersion.BuildInt;
+
+        public TimeSpan? TimeSpan;
+    }
+
+    [DBTableName("scrapping_loot_template")]
+    public class ScrappingLootTemplate : IDataModel
+    {
+        [DBFieldName("Entry", true)]
+        public uint Entry;
+
+        [DBFieldName("Item", true)]
+        public uint Item;
+
+        [DBFieldName("Reference", true)]
+        public int Reference;
+
+        [DBFieldName("Chance")]
+        public float Chance;
+
+        [DBFieldName("QuestRequired")]
+        public bool QuestRequired;
+
+        [DBFieldName("LootMode")]
+        public ushort LootMode;
+
+        [DBFieldName("GroupId")]
+        public byte GroupId;
+
+        [DBFieldName("MinCount")]
+        public byte MinCount;
+
+        [DBFieldName("MaxCount")]
+        public byte MaxCount;
+
+        [DBFieldName("Comment")]
+        public string Comment;
+
+        [DBFieldName("VerifiedBuild")]
+        public int VerifiedBuild = ClientVersion.BuildInt;
+
+        public TimeSpan? TimeSpan;
+    }
+
+    [DBTableName("item_loot_template")]
+    public class ItemLootTemplate : IDataModel
+    {
+        [DBFieldName("Entry", true)]
+        public uint Entry;
+
+        [DBFieldName("Item", true)]
+        public uint Item;
+
+        [DBFieldName("Reference", true)]
+        public int Reference;
+
+        [DBFieldName("Chance")]
+        public float Chance;
+
+        [DBFieldName("QuestRequired")]
+        public bool QuestRequired;
+
+        [DBFieldName("LootMode")]
+        public ushort LootMode;
+
+        [DBFieldName("GroupId")]
+        public byte GroupId;
+
+        [DBFieldName("MinCount")]
+        public byte MinCount;
+
+        [DBFieldName("MaxCount")]
+        public byte MaxCount;
+
+        [DBFieldName("Comment")]
+        public string Comment;
+
+        [DBFieldName("VerifiedBuild")]
+        public int VerifiedBuild = ClientVersion.BuildInt;
+
+        public TimeSpan? TimeSpan;
+    }
+
+    [DBTableName("prospecting_loot_template")]
+    public class ProspectingLootTemplate : IDataModel
+    {
+        [DBFieldName("Entry", true)]
+        public uint Entry;
+
+        [DBFieldName("Item", true)]
+        public uint Item;
+
+        [DBFieldName("Reference", true)]
+        public int Reference;
+
+        [DBFieldName("Chance")]
+        public float Chance;
+
+        [DBFieldName("QuestRequired")]
+        public bool QuestRequired;
+
+        [DBFieldName("LootMode")]
+        public ushort LootMode;
+
+        [DBFieldName("GroupId")]
+        public byte GroupId;
+
+        [DBFieldName("MinCount")]
+        public byte MinCount;
+
+        [DBFieldName("MaxCount")]
+        public byte MaxCount;
+
+        [DBFieldName("Comment")]
+        public string Comment;
+
+        [DBFieldName("VerifiedBuild")]
+        public int VerifiedBuild = ClientVersion.BuildInt;
+
+        public TimeSpan? TimeSpan;
+    }
+
+    [DBTableName("milling_loot_template")]
+    public class MillingLootTemplate : IDataModel
+    {
+        [DBFieldName("Entry", true)]
+        public uint Entry;
+
+        [DBFieldName("Item", true)]
+        public uint Item;
+
+        [DBFieldName("Reference", true)]
+        public int Reference;
+
+        [DBFieldName("Chance")]
+        public float Chance;
+
+        [DBFieldName("QuestRequired")]
+        public bool QuestRequired;
+
+        [DBFieldName("LootMode")]
+        public ushort LootMode;
+
+        [DBFieldName("GroupId")]
+        public byte GroupId;
+
+        [DBFieldName("MinCount")]
+        public byte MinCount;
+
+        [DBFieldName("MaxCount")]
+        public byte MaxCount;
+
+        [DBFieldName("Comment")]
+        public string Comment;
+
+        [DBFieldName("VerifiedBuild")]
+        public int VerifiedBuild = ClientVersion.BuildInt;
+
+        public TimeSpan? TimeSpan;
+    }
+
+    [DBTableName("spell_loot_template")]
+    public class SpellLootTemplate : IDataModel
+    {
+        [DBFieldName("Entry", true)]
+        public uint Entry;
+
+        [DBFieldName("Item", true)]
+        public uint Item;
+
+        [DBFieldName("Reference", true)]
+        public int Reference;
+
+        [DBFieldName("Chance")]
+        public float Chance;
+
+        [DBFieldName("QuestRequired")]
+        public bool QuestRequired;
+
+        [DBFieldName("LootMode")]
+        public ushort LootMode;
+
+        [DBFieldName("GroupId")]
+        public byte GroupId;
+
+        [DBFieldName("MinCount")]
+        public byte MinCount;
+
+        [DBFieldName("MaxCount")]
+        public byte MaxCount;
+
+        [DBFieldName("Comment")]
+        public string Comment;
+
+        [DBFieldName("VerifiedBuild")]
+        public int VerifiedBuild = ClientVersion.BuildInt;
+
+        public TimeSpan? TimeSpan;
+    }
+}
