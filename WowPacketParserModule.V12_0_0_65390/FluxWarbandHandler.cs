83f8053e74b48a429101b84bc979276b37689631
Fluxurion <zoltan@fatalerror.hu>
2026-05-13 10:31:21 +0200

    Refactor HandleSetupWarbandGroups to improve member reading logic and enhance name length handling

--- WowPacketParserModule.V12_0_0_65390/FluxWarbandHandler.cs
+++ WowPacketParserModule.V12_0_0_65390/FluxWarbandHandler.cs
@@ -21,33 +21,41 @@ namespace WowPacketParserModule.V12_0_0_65390.Parsers
         [Parser(Opcode.CMSG_SETUP_WARBAND_GROUPS)]
         public static void HandleSetupWarbandGroups(Packet packet)
         {
-            packet.ResetBitReader();
+            // CDataStore::WriteBits5
             var groupCount = packet.ReadBits("GroupCount", 5);
-            packet.ResetBitReader();
+            packet.ResetBitReader(); // FlushBits
 
             for (var i = 0; i < groupCount; i++)
             {
                 packet.ReadUInt64("GroupID", i);
-                packet.ReadByte("OrderIndex", i);
-                packet.ReadUInt32("WarbandSceneID", i);
-                packet.ReadUInt32("Flags", i);
-                packet.ReadUInt32("Unk", i);
-
-                var memberCount = packet.ReadUInt32("MemberCount", i);
+                packet.ReadByte("OrderIndex", i); // v8 + 8
+                packet.ReadUInt32("WarbandSceneID", i); // v8 + 268
+                packet.ReadUInt32("Flags", i); // v8 + 272
+                packet.ReadUInt32("Unk3", i); // v8 + 276 (Could be IconID?)
+                
+                var memberCount = packet.ReadUInt32("MemberCount", i); // v8 + 288
 
                 for (var j = 0; j < memberCount; j++)
-                    ReadWarbandGroupMember(packet, i, j);
+                {
+                    packet.ReadUInt32("SlotIndex", i, j); // v11 + 0
+                    var memberType = packet.ReadUInt32("MemberType", i, j); // v11 + 4
+                    packet.ReadUInt32("Unk4", i, j); // v11 + 8
 
-                var nameLengthHigh = packet.ReadByte("NameLengthHigh", i);
+                    if (memberType == 0)
+                        packet.ReadPackedGuid128("MemberGUID", i, j); // v11 + 16
+                }
 
-                packet.ResetBitReader();
-                var nameLengthLowBit = packet.ReadBit("NameLengthLowBit", i);
-                packet.ReadBit("NameUnkBit", i);
-                packet.ResetBitReader();
+                // Logic for Name Length (v13 >> 1 and v13 & 1)
+                // This is a 9-bit read in total.
+                var nameLength = packet.ReadBits("NameLength", 9, i);
+                packet.ResetBitReader(); // FlushBits
 
-                var nameLength = (nameLengthHigh << 1) | (nameLengthLowBit ? 1 : 0);
                 if (nameLength > 0)
+                {
+                    // Note: The assembly uses WriteData with v14 (the raw length)
+                    // ReadWoWString usually expects a length or reads until null.
                     packet.ReadWoWString("GroupName", nameLength, i);
+                }
             }
         }
     }
